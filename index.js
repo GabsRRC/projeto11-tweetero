fc00f2ef8c3a89e6a338c1c62f5c275cfe713af3
gabs <gabriellerrc@gmail.com>
2022-06-20 13:52:55 -0300

    server up

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from "express"
+import cors from "cors"
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+let users = [];
+let tweets = [];
+
+app.post("/sign-up", (req, res) => {
+  const { username, avatar } = req.body;
+  const user = {
+    username: username,
+    avatar: avatar,
+  };
+
+  users.push(user);
+  res.send("Ok");
+});
+
+app.post("/tweets", (req, res) => {
+  const { username, tweet } = req.body;
+  const tweetUser = users.find((user) => user.username === username);
+  
+  const tweetPost = { 
+    username: username,
+    avatar: tweetUser.avatar,
+    tweet: tweet,
+  };
+
+  tweets.push(tweetPost); 
+  res.send("Ok");
+})
+
+app.get("/tweets", (req, res) => {
+    const lastTweets = tweets.slice(-10);
+    lastTweets.reverse();
+    res.send(lastTweets); 
+})
+
+
+app.listen(5000, ()=> {
+    console.log("Server up")
+})
